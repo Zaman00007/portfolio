cb61c169a4b06debaf4ac8100a6546e8d299d444
Zaman00007 <shahidzaman75724@gmail.com>
2024-01-28 05:06:41 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 function Navbar() {
   return (
     <div>
-      <h1>Navbar</h1>
+      <header className='header'>
+        <NavLink to='/' className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
+            <p className='blue-gradient_text' >SZ</p>
+        </NavLink>
+        <nav className='flex text-lg gap-7 font-medium'>
+            <NavLink to='/about' className = {({isActive})=>isActive ? 'text-blue-500' : 'text-black'}>
+                About
+            </NavLink>
+            <NavLink to='/projects' className = {({isActive})=>isActive ? 'text-blue-500' : 'text-black'}>
+                Projects
+            </NavLink>
+            <NavLink to='/contacts' className = {({isActive})=>isActive ? 'text-blue-500' : 'text-black'}>
+                Contact
+            </NavLink>
+                
+            
+        </nav>
+      </header>
     </div>
   )
 }
