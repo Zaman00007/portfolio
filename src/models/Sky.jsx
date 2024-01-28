050f4ec070fac8c49bef606c6309e36211f76dde
Zaman00007 <shahidzaman75724@gmail.com>
2024-01-28 06:48:53 +0530

    sky

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,9 +1,12 @@
+import { useGLTF } from '@react-three/drei';
 import React from 'react'
+import skyScene from '../../public/assets/3d/sky.glb';
 
 function Sky() {
+  const sky = useGLTF(skyScene);
   return (
     <mesh>
-        
+        <primitive object={sky.scene} />
     </mesh>
   )
 }
