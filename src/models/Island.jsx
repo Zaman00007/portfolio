7f4bf521f0c6cdb8e3722e526cd72cbe7a43cc43
Zaman00007 <shahidzaman75724@gmail.com>
2024-01-28 16:54:03 +0530

    path

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -9,7 +9,7 @@ Title: Fox's islands
 import React, { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import islandScene from "../../public/assets/3d/island.glb";
+import islandScene from "../assets/3d/island.glb";
 import {a} from '@react-spring/three'
 // import { is } from "@react-three/fiber/dist/declarations/src/core/utils";
 
