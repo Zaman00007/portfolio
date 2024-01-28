776f07679bda9d52d4a26bf06c3915ed49026051
Zaman00007 <shahidzaman75724@gmail.com>
2024-01-28 06:30:05 +0530

    adjustScreenSize

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -6,57 +6,55 @@ Source: https://sketchfab.com/3d-models/foxs-islands-163b68e09fcc47618450150be77
 Title: Fox's islands
 */
 
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import islandScene from "../../public/assets/3d/island.glb";
+import {a} from '@react-spring/three'
+// import { is } from "@react-three/fiber/dist/declarations/src/core/utils";
 
-export function Model(props) {
-  const { nodes, materials } = useGLTF("/island.glb");
+function Island (props) {
+  const { nodes, materials } = useGLTF(islandScene);
+  const islandRef = useRef();
   return (
-    <group {...props} dispose={null}>
+    <a.group ref={islandRef} {...props} >
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface944_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface945_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
+        
         geometry={nodes.polySurface946_tree2_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
+        
         geometry={nodes.polySurface947_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
+        
         geometry={nodes.polySurface948_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
+        
         geometry={nodes.polySurface949_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
+        
         geometry={nodes.pCube11_rocks1_0.geometry}
         material={materials.PaletteMaterial001}
       />
-    </group>
+    </a.group>
   );
 }
 
 useGLTF.preload("/island.glb");
+
+export default Island;
