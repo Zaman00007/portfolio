5db37c2592bddfa71c9880c04754dd569eff16d6
Zaman00007 <shahidzaman75724@gmail.com>
2024-01-28 05:34:04 +0530

    Loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import "./index.css"; 
+import "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
@@ -9,10 +14,10 @@ function App() {
       <Router>
          <Navbar />
         <Routes>
-          <Route path="/" element={"Home"} />
-          <Route path="/about" element={"About"} />
-          <Route path="/projects" element={"Projects"} />
-          <Route path="/contacts" element={"Contacts"} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/contacts" element={<Contact/>} />
 
         </Routes>
       </Router>
